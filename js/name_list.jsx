00875f57591f3d40c86ddc0d718a1a38649c8339
Mickey <ekey.huang@gmail.com>
2017-02-17 22:08:42 -0500

    try react

--- js/name_list.jsx
+++ js/name_list.jsx
@@ -1,1 +1,25 @@
-ReactDOM.render(<div>Whatever</div>,document.getElementById('react'));
+var NameList = React.createClass({
+  getInitialState: function(){
+    return {
+      contacts: [
+        {
+          name: '时瑞琪',
+          phone: 13051162287
+        },
+        {
+          name: '胡若婕(Banana)',
+          phone: 18515155564
+        }
+      ]
+    };
+  },
+  render: function(){
+    return (
+      <a href='contacts/'+{this.state.contacts.phone}+'.html'>
+          <li>{this.state.contacts.name}</li>
+      </a>
+    );
+  }
+});
+
+ReactDOM.render(,document.getElementById('test'));
